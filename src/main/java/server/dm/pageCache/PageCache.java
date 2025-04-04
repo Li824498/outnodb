4b78d3fda2902ed037100f8880abf6c0c3010ed1
Li824498 <1691508029@qq.com>
2025-04-04 17:21:21 +0800

    dm05:pageCache类以及接口类

--- src/main/java/server/dm/pageCache/PageCache.java
+++ src/main/java/server/dm/pageCache/PageCache.java
@@ -1,9 +1,64 @@
 package server.dm.pageCache;
 
+import common.Error;
 import server.dm.page.Page;
+import server.utils.Panic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 public interface PageCache {
     final static int PAGE_SIZE = 1 << 13;
 
+    int newPage(byte[] initData);
+    Page getPage(int pgno);
     void release(Page page);
+
+    void close();
+
+    void truncateyByPgno(int maxPgno);
+    int getPageNumber();
+    void flushPage(Page page);
+
+    public static PageCacheImpl create(String path, long memory) {
+        File file = new File(path + PageCacheImpl.DB_SUFFIX);
+        try {
+            if(!file.createNewFile()){
+                Panic.panic(Error.FileExistsException());
+            }
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+
+        RandomAccessFile raf = null;
+        FileChannel fc = null;
+
+        try {
+            raf = new RandomAccessFile(file, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+
+        return new PageCacheImpl(raf, fc, (int) (memory/PAGE_SIZE));
+    }
+
+    public static PageCacheImpl open(String path, long memory) {
+        File file = new File(path + PageCacheImpl.DB_SUFFIX);
+        if(!file.exists()) {
+            Panic.panic(Error.FileNotExistsException());
+        }
+        RandomAccessFile raf = null;
+        FileChannel fc = null;
+        try {
+            raf = new RandomAccessFile(file, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        return new PageCacheImpl(raf, fc, (int) (memory/PAGE_SIZE));
+    }
 }
--- src/main/java/server/dm/pageCache/PageCacheImpl.java
+++ src/main/java/server/dm/pageCache/PageCacheImpl.java
@@ -2,19 +2,135 @@ package server.dm.pageCache;
 
 import server.common.AbstractCache;
 import server.dm.page.Page;
+import server.dm.page.PageImpl;
+import server.utils.Panic;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 页缓存类，负责页、页缓存的管理
+ */
 public class PageCacheImpl extends AbstractCache<Page> implements PageCache{
-    public PageCacheImpl(int maxResource) {
+    public static final String DB_SUFFIX = ".db";
+
+    private RandomAccessFile raf;
+    private FileChannel fc;
+    private Lock lock;
+
+    private AtomicInteger pageNumbers;
+
+    public PageCacheImpl(RandomAccessFile raf, FileChannel fc, int maxResource) {
         super(maxResource);
+        this.raf = raf;
+        this.fc = fc;
+        this.lock = new ReentrantLock();
+        long length = 0;
+        try {
+            length = raf.length();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+
+        this.pageNumbers = new AtomicInteger((int) (length / PAGE_SIZE));
     }
 
     @Override
     protected Page getForCache(long key) throws Exception {
-        return null;
+        int pgno = (int) key;
+        long pageOffset = pageOffset(pgno);
+        lock.lock();
+        ByteBuffer buf = ByteBuffer.allocate(PAGE_SIZE);
+        try {
+            fc.position(pageOffset);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        } finally {
+            lock.unlock();
+        }
+        return new PageImpl(pgno, buf.array(), this);
     }
 
     @Override
-    protected void releaseForCache(Page obj) {
+    protected void releaseForCache(Page page) {
+        if(page.isDirty()) {
+            flush(page);
+            page.setDirty(false);
+        }
+    }
 
+    @Override
+    public int newPage(byte[] initData) {
+        int pgno = pageNumbers.incrementAndGet();
+        PageImpl page = new PageImpl(pgno, initData, null);
+        flush(page);
+        return pgno;
+    }
+
+    public void flush(Page page) {
+        int pgno = page.getPageNumber();
+        long offset = pageOffset(pgno);
+
+        lock.lock();
+        try {
+            fc.position(offset);
+            fc.write(ByteBuffer.wrap(page.getDate()));
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private long pageOffset(int pgno) {
+        return (long) (pgno - 1) * PAGE_SIZE;
+    }
+
+    @Override
+    public Page getPage(int pgno) {
+        return super.get(pgno);
+    }
+
+    @Override
+    public void release(Page page) {
+        super.release(page.getPageNumber());
+    }
+
+    @Override
+    public void close() {
+        try {
+            raf.close();
+            fc.close();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    @Override
+    public void truncateyByPgno(int maxPgno) {
+        long maxSize = pageOffset(maxPgno + 1);
+        try {
+            raf.setLength(maxSize);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        pageNumbers.set(maxPgno);
+    }
+
+    @Override
+    public int getPageNumber() {
+        return pageNumbers.intValue();
+    }
+
+    @Override
+    public void flushPage(Page page) {
+        flush(page);
     }
 }
