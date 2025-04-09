3235452084dd0ad3c6e4366cb7cb74ec335caf8b
Li824498 <1691508029@qq.com>
2025-04-09 19:27:45 +0800

    dm09:页索引类多线程优化
    1.优化锁细粒度至桶级单位，并抽象全局锁（模仿意向锁）来保证add与select互斥
    2.确保不会发生死锁：
    /**
     * 1. 互斥条件：每个锁（locks[number] 和 lockAll）只能由一个线程持有，其他线程不能同时访问。
     * 2. 占有且等待：线程可能在持有一个锁时等待其他锁。
     * 3. 不剥夺条件：锁不能被其他线程强行夺走，必须由持有线程释放。
     * 4. 循环等待条件：线程没有形成相互等待的循环，因此不会发生死锁。
     */

--- src/main/java/server/dm/pageIndex/PageIndex.java
+++ src/main/java/server/dm/pageIndex/PageIndex.java
@@ -9,36 +9,52 @@ import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 1. 互斥条件：每个锁（locks[number] 和 lockAll）只能由一个线程持有，其他线程不能同时访问。
+ * 2. 占有且等待：线程可能在持有一个锁时等待其他锁。
+ * 3. 不剥夺条件：锁不能被其他线程强行夺走，必须由持有线程释放。
+ * 4. 循环等待条件：线程没有形成相互等待的循环，因此不会发生死锁。
+ */
+
 public class PageIndex {
     private static final int INTERVALS_NO = 40;
     private static final int THRESHOLD = PageCache.PAGE_SIZE / INTERVALS_NO;
 
     private List<PageInfo>[] lists;
-    private Lock lock;
+    private Lock[] locks;
+    private Lock lockAll;
 
     public PageIndex() {
-        lock = new ReentrantLock();
         lists = new List[INTERVALS_NO + 1];
         for(List list : lists) {
             list = new ArrayList();
         }
+        locks = new ReentrantLock[INTERVALS_NO + 1];
+        lockAll = new ReentrantLock();
     }
 
     public void add(Page page, int freeSpace) {
         int number = freeSpace / THRESHOLD;
-        lock.lock();
+        locks[number].lock();
         try {
-            lists[number].add(new PageInfo(page, freeSpace));
+            lockAll.lock();
+            try {
+                lists[number].add(new PageInfo(page, freeSpace));
+            } catch (Exception e) {
+                Panic.panic(e);
+            } finally {
+                lockAll.unlock();
+            }
         } catch (Exception e) {
             Panic.panic(e);
         } finally {
-            lock.unlock();
+            locks[number].unlock();
         }
     }
 
     public PageInfo select(int freeSpace) {
         int number = freeSpace / THRESHOLD;
-        lock.lock();
+        lockAll.lock();
         try {
             if (number < INTERVALS_NO) number++;
             while(number <= INTERVALS_NO) {
@@ -51,7 +67,7 @@ public class PageIndex {
             }
             return null;
         } finally {
-            lock.unlock();
+            lockAll.unlock();
         }
     }
 }
