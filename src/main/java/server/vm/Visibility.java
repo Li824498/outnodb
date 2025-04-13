3885f0df40aa447fd4b9e7d7f320311f00ab470c
Li824498 <1691508029@qq.com>
2025-04-13 15:37:10 +0800

    vm07:补充注释

--- src/main/java/server/vm/Visibility.java
+++ src/main/java/server/vm/Visibility.java
@@ -4,9 +4,20 @@ import server.tm.TransactionManager;
 
 public class Visibility {
 
-    // todo 版本跳跃api
+    /**
+     * mysql是怎么解决并发读写同一行数据的？使用for update显式加锁\写操作自动加锁，强制在这几个状态时，令逻辑上的数据与物理上的数据同步，实际上就是禁止发生逻辑物理不一致的行为（在间隙写入数据）
+     * 如何解决间隙数据（幻读）？间隙锁
+     * outnodb是怎么解决发读写同一行数据的？试用版本跳跃检测
+     * 如何解决间隙数据（幻读）？未解决，会有这种问题？ todo 看看问题
+     * 禁止用户发生这种行为：逻辑物理不一致，检测到这种行为就报错
+     * @param tm
+     * @param t
+     * @param e
+     * @return
+     */
     public static boolean isVersionSkip(TransactionManager tm, Transaction t, Entry entry) {
         long xmax = entry.getXmax();
+        // 0:读已提交 1:可重复读
         if(t.level == 0) {
             return false;
         } else {
