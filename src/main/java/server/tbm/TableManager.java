bd040ff74d8b510f657dc338431c3bc8289736f2
Li824498 <1691508029@qq.com>
2025-04-15 16:16:52 +0800

    tbm03:tbm接口框架

---
+++ src/main/java/server/tbm/TableManager.java
@@ -0,0 +1,32 @@
+package server.tbm;
+
+import server.dm.DataManager;
+import server.parser.statement.*;
+import server.utils.Parser;
+import server.vm.VersionManager;
+
+public interface TableManager {
+    BeginRes begin(Begin begin);
+    byte[] commit(long xid) throws Exception;
+    byte[] abort(long xid);
+
+    byte[] show(long xid);
+    byte[] create(long xid, Create create) throws Exception;
+
+    byte[] insert(long xid, Insert insert) throws Exception;
+    byte[] read(long xid, Select select) throws Exception;
+    byte[] update(long xid, Update update) throws Exception;
+    byte[] delete(long xid, Delete delete) throws Exception;
+
+
+    public static TableManager create(String path, VersionManager vm, DataManager dm) {
+        Booter booter = Booter.create(path);
+        booter.update(Parser.long2byte(0));
+        return new TableManagerImpl(vm, dm, booter);
+    }
+
+    public static TableManager open(String path, VersionManager vm, DataManager dm) {
+        Booter booter = Booter.open(path);
+        return new TableManagerImpl(vm, dm, booter);
+    }
+}
---
+++ src/main/java/server/tbm/TableManagerImpl.java
@@ -0,0 +1,8 @@
+package server.tbm;
+
+import server.dm.DataManager;
+import server.vm.VersionManager;
+
+public class TableManagerImpl implements TableManager {
+
+}
